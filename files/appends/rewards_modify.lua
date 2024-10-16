73e4d3cd86a3f32dd5184a10cc21a2a6c389028f
nozwock <57829219+nozwock@users.noreply.github.com>
2024-10-16 16:42:48 +0530

    Fix import paths

--- files/appends/rewards_modify.lua
+++ files/appends/rewards_modify.lua
@@ -1,5 +1,5 @@
-local const = dofile_once("mods/noita/meta-leveling-reward-options/files/const.lua") ---@type const
-local utils = dofile_once("mods/noita/meta-leveling-reward-options/files/utils.lua") ---@type utils
+local const = dofile_once("mods/meta-leveling-reward-options/files/const.lua") ---@type const
+local utils = dofile_once("mods/meta-leveling-reward-options/files/utils.lua") ---@type utils
 
 local rewards_deck = dofile_once("mods/meta_leveling/files/scripts/classes/private/rewards_deck.lua") ---@type rewards_deck
 for _, reward in ipairs(rewards_deck.reward_definition_list) do
@@ -16,7 +16,7 @@ for _, reward in ipairs(rewards_deck.reward_definition_list) do
     reward.mlro_state.custom_check = true --- reward always available
     reward.custom_check = function()
       local enable = ModSettingGet(utils:ResolveModSettingId(const.reward_setting_prefix ..
-      reward.id .. const.reward_setting_suffix.enable))
+        reward.id .. const.reward_setting_suffix.enable))
       if enable ~= nil then
         return enable
       end
